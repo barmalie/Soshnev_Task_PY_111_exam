d5c13436893d078e43ff2251dfeff481779b67ef
Ruslan <6r0d5k1y@gmail.com>
2022-10-23 00:52:18 +0300

    commit

---
+++ task_3.py
@@ -0,0 +1,33 @@
+import random
+def print_my_call():
+    count = []
+    range_ = range(random.randint(1, 23))
+    for i in range_:
+        start_time = random.randint(0, 23)
+        end_time = random.randint(1, 24)
+        zayavka = (start_time, end_time)
+        if start_time < end_time:
+            count.append(zayavka)
+
+    sort_index = sorted(count, key=lambda zayavka :zayavka[0])
+    #print(sort_index)
+    n = 0
+    list_wish = []
+    for wish in sort_index:
+        if sort_index[n+1][0] > sort_index[n][1]:
+
+            if sort_index[n+1][1] > sort_index[n][1] and sort_index[n+1][0] > sort_index[n][0]:
+                list_wish.append(wish)
+                #print(list_wish)
+            else:
+                print(wish)
+
+        if sort_index[n+1][0] > sort_index[n][1]:
+
+        #print(set(list_wish))
+            print(wish)
+
+    #print(list_wish[n])
+
+
+print_my_call()
