41de94bf9b508d8a5faa2574de39eb977154a1ae
Ruslan <6r0d5k1y@gmail.com>
2022-10-23 00:22:05 +0300

    commit

---
+++ task_1.py
@@ -0,0 +1,42 @@
+text = "flfkxkckg lglglglvl glofkg, fkfkfkfrdr."
+modify_text = text.replace(" ", "")
+modify_text = modify_text.replace(",", "")
+modify_text = modify_text.replace(".", "")
+
+print(len(modify_text)//2)
+count_slog = len(modify_text) // 2
+for slog in range(1, count_slog + 1):
+    list = []# список слогов
+    list.append(slog)
+
+
+    count_pleyer = 3
+    list_count_player = []
+    assert count_pleyer < count_slog
+    for player in range(1, count_pleyer+1):
+        #list2 = []# список игроков
+        list_count_player.append(player)
+    for player in list_count_player:
+
+        players = len(list_count_player)
+        counter = 0
+        pointer = 1
+    # while slog > player:
+    #     #print(list_count_player)
+    #     num = count_slog % count_pleyer
+    #     if player == 1:
+    #         print("победитель", player)
+    #     else:
+    #         list_count_player.pop()
+    #         pointer -= 1
+
+    while  player > 1:
+        if count_slog % count_pleyer == 0:
+            list_count_player.pop()
+            if player == 1:
+                print("победитель", player)
+            else:
+                list_count_player.pop()
+                pointer -= 1
+
+print(list_count_player)
